43c6f26ddbbc272ad5e12dc1ee45fdcea4fa61ef
Hamza J <104732125+HJ1111111@users.noreply.github.com>
2024-01-04 15:11:57 +0100

    new FastAPI project created with endpoints

---
+++ README.md
@@ -0,0 +1,36 @@
+# FastAPI_Project-Backend
+
+## Introduction
+
+A FastAPI sample project for a Backend that displays the creation of a Virtual Environment, Project structure, and FastAPI applications.
+
+---
+
+## Getting started
+
+### Development
+
+This application requires you to have the following tools installed:
+
+- Python 3.11+
+
+  Create a Virtual Environment (Optional but Recommended):
+
+- python -m venv fastapi-env
+
+- Install FastAPI and Uvicorn:
+  pip install fastapi uvicorn
+
+Run the project with
+
+```sh
+uvicorn main:app --reload
+```
+
+- Uvicorn running on http://127.0.0.1:8000
+
+### Documentation
+
+Automatic interactive API documentation (provided by Swagger UI):
+
+- http://127.0.0.1:8000/docs
---
+++ main.py
@@ -0,0 +1,43 @@
+"""Entry point for the API."""
+
+import random
+from fastapi import FastAPI
+from typing import Optional
+from datetime import datetime
+
+app = FastAPI()
+
+# class ReadAllProjects():
+#     """This base model for reading all projects."""
+
+#     name: str
+#     description: Optional[str]
+#     creation_date: Optional[datetime]
+
+
+@app.get('/todo', tags=['Read All Projects'])
+async def get_projects() -> dict:
+    return{"data": todos}
+
+
+
+@app.post("/todo", tags=["Read All Projects"])
+async def add_project(todo:dict) -> dict:
+    todos.append(todo)
+    return {
+        "data": "A project has beed added"
+    }
+
+
+todos = [
+    {
+        "name": "Demo",
+        "Description": "this is the 1st project",
+        "Creation_Date": "04.01.2024"
+    },
+    {
+        "name": "CleanCode",
+        "Description": "this is the 2nd project",
+        "Creation_Date": "04.01.2024"
+    }
+]
