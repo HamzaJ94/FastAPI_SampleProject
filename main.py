c75a68ee3596dcd2a88af27fec25e6a0e34c695c
Hamza J <104732125+HJ1111111@users.noreply.github.com>
2024-01-04 16:14:46 +0100

    HTTP requests added and defined for the endpoint

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ from datetime import datetime
 
 app = FastAPI()
 
-# class ReadAllProjects():
-#     """This base model for reading all projects."""
-
-#     name: str
-#     description: Optional[str]
-#     creation_date: Optional[datetime]
-
 
 @app.get('/todo', tags=['Read All Projects'])
 async def get_projects() -> dict:
@@ -22,20 +15,45 @@ async def get_projects() -> dict:
 
 
 @app.post("/todo", tags=["Read All Projects"])
-async def add_project(todo:dict) -> dict:
+async def add_projects(todo:dict) -> dict:
     todos.append(todo)
     return {
         "data": "A project has beed added"
     }
 
+@app.put("/todo/{id}", tags=["Read All Projects"])
+async def update_projects(id: int, body: dict) -> dict:
+    for todo in todos:
+        if int((todo['id'])) == id:
+            todo['Description'] = body ['Description']
+            return{
+                "data": f"Project with id {id} has been updated"
+            }
+        return{
+            "data": f"Project with this id number {id} was not found!"
+        }
+
+@app.delete("/todo/{id}", tags=["Read All Projects"])
+async def delete_projects(id:int) -> dict:
+    for todo in todos:
+        if ((todo["id"])) == id:
+            todos.remove(todo)
+            return{
+                "data": f"Project with id {id} has been deleted"
+            }
+        return{
+                 "data": f"Project with id {id} wasn't found!"
+        }
 
 todos = [
     {
+        "id": "1",
         "name": "Project 1",
         "Description": "this is the 1st project",
         "Creation_Date": "04.01.2024"
     },
     {
+        "id": "2",
         "name": "Project 2",
         "Description": "this is the 2nd project",
         "Creation_Date": "04.01.2024"
