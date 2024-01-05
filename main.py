26df8b4f46094d1743c9e8ef552abe0068a291d7
Hamza J <104732125+HJ1111111@users.noreply.github.com>
2024-01-05 11:25:56 +0100

    code cleaning

--- main.py
+++ main.py
@@ -1,49 +1,48 @@
 """Entry point for the API."""
 
-import random
 from fastapi import FastAPI
-from typing import Optional
-from datetime import datetime
 
 app = FastAPI()
 
 
 @app.get('/todo', tags=['Read All Projects'])
 async def get_projects() -> dict:
-    return{"data": todos}
-
+    return {"data": todos}
 
 
 @app.post("/todo", tags=["Read All Projects"])
-async def add_projects(todo:dict) -> dict:
+async def add_projects(todo: dict) -> dict:
     todos.append(todo)
     return {
-        "data": "A project has beed added"
+        "data": "A project has been added"
     }
+
 
 @app.put("/todo/{id}", tags=["Read All Projects"])
 async def update_projects(id: int, body: dict) -> dict:
     for todo in todos:
         if int((todo['id'])) == id:
-            todo['Description'] = body ['Description']
-            return{
+            todo['Description'] = body['Description']
+            return {
                 "data": f"Project with id {id} has been updated"
             }
-        return{
+        return {
             "data": f"Project with this id number {id} was not found!"
         }
 
+
 @app.delete("/todo/{id}", tags=["Read All Projects"])
-async def delete_projects(id:int) -> dict:
+async def delete_projects(id: int) -> dict:
     for todo in todos:
-        if ((todo["id"])) == id:
+        if (todo["id"]) == id:
             todos.remove(todo)
-            return{
+            return {
                 "data": f"Project with id {id} has been deleted"
             }
-        return{
-                 "data": f"Project with id {id} wasn't found!"
+        return {
+            "data": f"Project with id {id} wasn't found!"
         }
+
 
 todos = [
     {
